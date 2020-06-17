9ed1ede88cf49035ff6a4d329ef3fce8138739fa
Leon Arndt <neotalon27@gmail.com>
2020-06-17 13:01:31 +0200

    Init population

---
+++ Assets/Permutation/Scripts/PermutationCheck.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+/// <summary>
+/// Compare two strings to identify whether a permutation exists
+/// </summary>
+public class PermutationCheck
+{
+
+    /// <summary>
+    /// Compare the string given in the UI against the string set in the GameObject
+    /// </summary>
+    /// <param name="given">string given in the UI</param>
+    public static bool IsPermutation(string baseString, string given)
+    {
+        /*
+         * for strings to be a permutation they must contain the same letters the same number of times.
+         * A simple way to check this is to try to subtract letters 
+         * from the given string and exit early (return false) if not possible.
+         * But first off, the length of the strings needs to match
+         */
+
+        //exit early if the string lengths do not match
+        if (given.Length != baseString.Length)
+        {
+            return false;
+        }
+
+        //change both strings to lowercase to avoid casing differences
+        baseString = baseString.ToLower();
+        given = given.ToLower();
+
+        //make sure that the original string remains unaltered
+        char[] charArray = given.ToCharArray();
+        List<char> givenCopy = charArray.ToList<char>();
+
+        foreach (char letter in baseString)
+        {
+            //does the given string contain the letter?
+            if (givenCopy.Contains(letter))
+            {
+                //remove the letter from the given copy
+                givenCopy.Remove(letter);
+            }
+            else
+            {
+                //since we are missing a character, a permutation can not exist
+                return false;
+            }
+        }
+
+        //we have a permutation since all required letters were removed and string length matches
+        return true;
+    }
+}
---
+++ Assets/Permutation/Scripts/PermutationUi.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Handle the UI for the permutation check
+/// </summary>
+public class PermutationUi : MonoBehaviour {
+    //local fields to cache strings
+    private string topString = string.Empty;
+    private string bottomString = string.Empty;
+
+    [SerializeField]
+    Text resultText;
+
+    //game view feedback for matches
+    private readonly string ifMatch = "The strings are permutations";
+    private readonly string ifNoMatch = "The strings are not permutations";
+
+    /// <summary>
+    /// Compare the strings when the input field text is changed by the user
+    /// </summary>
+    /// <param name="given"></param>
+    public void CompareStrings(string top, string bottom)
+    {
+        bool match = PermutationCheck.IsPermutation(top, bottom);
+
+        if (match)
+        {
+            resultText.text = ifMatch;
+        }
+        else
+        {
+            resultText.text = ifNoMatch;
+        }
+    }
+
+    #region INSPECTOR_FUNCTIONS
+    public void UpdateTop(string replacement)
+    {
+        topString = replacement;
+        CompareStrings(topString, bottomString);
+    }
+
+    public void UpdateBottom(string replacement)
+    {
+        bottomString = replacement;
+        CompareStrings(topString, bottomString);
+    }
+    #endregion
+}
