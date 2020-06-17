1122de3980e350713317bd463f8d33a4d86caf5a
Leon Arndt <neotalon27@gmail.com>
2020-06-17 15:31:53 +0200

    Add dialogue system

---
+++ Assets/ShelfCounter/Register/Scripts/DialogueManager.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+/// <summary>
+/// Dialogue Manager written by Leon Arndt.
+/// Combines both Logic and UI. Uses data from the DialogueScene object.
+/// </summary>
+public class DialogueManager : MonoBehaviour
+{
+    public UnityEvent dialogueFinished;
+
+    [SerializeField]
+    private DialogueScene dialogueScene;
+
+    //private Translator translator;
+
+    private DialogueSegment currentDialogueSegment;
+    private string currentTranslatedDialogueText;
+
+    [SerializeField]
+    private Text dialogueText;
+
+    [SerializeField]
+    private GameObject dialogueBox;
+
+    public bool inDialogue;
+    private bool listeningForSkips = false;
+    private int currentDialogueSegmentIndex = 0;
+    private float startTime;
+    private const float lettersPerSecond = 32f;
+
+    private void Start()
+    {
+        HideTextBox();
+        if (dialogueFinished == null)
+            dialogueFinished = new UnityEvent();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (inDialogue)
+        {
+            int length = Mathf.Min(currentTranslatedDialogueText.Length, (int)(lettersPerSecond * (Time.time - startTime)));
+
+            //display letter after letter
+            if (dialogueText.text != currentTranslatedDialogueText)
+            {
+                dialogueText.text = currentTranslatedDialogueText.Substring(0, length);
+            }
+
+            //Skip dialogue text
+            if (GetValidSkip())
+            {
+                //show all text
+                if (dialogueText.text != currentTranslatedDialogueText)
+                {
+                    dialogueText.text = currentTranslatedDialogueText;
+                }
+                else
+                //if there are more dialogue segments to display, display a new one
+                if (currentDialogueSegmentIndex < dialogueScene.segments.Length - 1)
+                {
+                    currentDialogueSegmentIndex++;
+                    currentDialogueSegment = dialogueScene.segments[currentDialogueSegmentIndex];
+                    currentTranslatedDialogueText = currentDialogueSegment.dialogueString;
+                    ClearTextBox();
+                }
+                else
+                {
+                    //dialogue is finished
+                    if (dialogueFinished != null)
+                    {
+                        dialogueFinished.Invoke();
+                        dialogueFinished = null;
+                    }
+
+                    HideTextBox();
+                }
+            }
+        }
+    }
+
+    private bool GetValidSkip()
+    {
+        if (!listeningForSkips) return false;
+
+        return Input.GetKeyDown(KeyCode.Space)
+            || Input.GetMouseButtonDown(0);
+    }
+
+    private void ClearTextBox()
+    {
+        startTime = Time.time;
+        dialogueText.text = "";
+    }
+
+    public void LoadDialogueScene(DialogueScene newDialogueScene)
+    {
+        dialogueScene = newDialogueScene;
+    }
+
+    public void EnterDialogue()
+    {
+        listeningForSkips = false;
+        startTime = Time.time;
+        ClearTextBox();
+        currentDialogueSegmentIndex = 0;
+        currentDialogueSegment = dialogueScene.segments[0];
+        currentTranslatedDialogueText = currentDialogueSegment.dialogueString;
+        dialogueBox.SetActive(true);
+        inDialogue = true;
+
+        //start listening after 0.1f seconds
+        Invoke("StartListeningForSkips", 0.1f);
+    }
+
+    private void HideTextBox()
+    {
+        dialogueBox.SetActive(false);
+        inDialogue = false;
+    }
+
+    private void StartListeningForSkips()
+    {
+        listeningForSkips = true;
+    }
+}
+
+#region Data
+
+public struct DialogueSegment
+{
+    public string dialogueString;
+
+    public DialogueSegment(string dialogueString)
+    {
+        this.dialogueString = dialogueString;
+    }
+}
+
+public struct DialogueScene
+{
+    public DialogueSegment[] segments;
+
+    public DialogueScene(DialogueSegment[] segments)
+    {
+        this.segments = segments;
+    }
+}
+#endregion
--- Assets/ShelfCounter/Register/Scripts/Register.cs
+++ Assets/ShelfCounter/Register/Scripts/Register.cs
@@ -2,15 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Register : MonoBehaviour {
+/// <summary>
+/// The register can be clicked to display purchase information
+/// </summary>
+public class Register : Interactable {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public override void Interact()
+    {
+        NotifyOfBasketAndCosts();
+    }
+
+    /// <summary>
+    /// Tell the user what they are going to purchase and how much it will cost
+    /// </summary>
+    private void NotifyOfBasketAndCosts()
+    {
+        string notifyString = string.Empty;
+
+        //get total cost
+        Counter counter = FindObjectOfType<Counter>();
+        float totalPrice = counter.GetTotalCost();
+
+        //get items list
+        string purchases = "You are going to purchase ";
+
+
+        string costString = "Which will cost you " + totalPrice + " alltogether.";
+
+        //create dialogue scene by building it from dialogue segments
+        DialogueManager dialogue = FindObjectOfType<DialogueManager>();
+        DialogueSegment segment1 = new DialogueSegment(purchases);
+        DialogueSegment segment2 = new DialogueSegment(costString);
+
+        //combine the scene
+        DialogueScene scene = new DialogueScene(new DialogueSegment[] { segment1, segment2 });
+
+        //display the dialogue
+        dialogue.LoadDialogueScene(scene);
+        dialogue.EnterDialogue();
+    }
 }
