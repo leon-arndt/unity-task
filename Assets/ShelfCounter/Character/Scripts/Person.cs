5b24a6ac6aae41462bbe6b1aee27cecc0e1caf9f
Leon Arndt <neotalon27@gmail.com>
2020-06-17 15:10:54 +0200

    Add waving animation when person is clicked

---
+++ Assets/ShelfCounter/Character/Scripts/Person.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Use this class to add behavior to the person in the world
+/// </summary>
+public class Person : Interactable {
+    [SerializeField]
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public override void Interact()
+    {
+        Wave();
+    }
+
+    private void Wave()
+    {
+        anim.SetTrigger("Wave");
+    }
+}
